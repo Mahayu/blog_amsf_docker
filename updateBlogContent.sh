e3075f61f518038ae263abb029be681d5246b81f
mahayu <preloguend@gmail.com>
2022-05-15 18:26:31 +0000

    vhost update

--- updateBlogContent.sh
+++ updateBlogContent.sh
@@ -1,7 +1,9 @@
 docker compose up
 
-sudo docker cp blog-amsf-1:/app/_site/. ../dists #取出编译完毕的内容
+sudo docker cp blog-amsf-1:/app/_site/. ../dists/ #取出编译完毕的内容
 
-sudo rsync -a /home/iltti/amsf-blog/dists/. /var/www/html #  博客内容暂时先当首页
+sudo rsync -av --delete /home/iltti/amsf-blog/dists/. /var/www/html #  博客内容暂时先当首页
 
 sudo systemctl restart apache2
+
+sudo rm -rf ../dists/
