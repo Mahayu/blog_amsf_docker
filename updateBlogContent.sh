0f96da586c659e90d7d1e1285b4e7c5676a838e9
mahayu <preloguend@gmail.com>
2024-08-08 07:02:46 +0000

    update shell script

--- updateBlogContent.sh
+++ updateBlogContent.sh
@@ -1,6 +1,7 @@
-docker compose up
+sudo docker compose up
 sudo docker cp blog_amsf_docker-amsf-1:/app/_site/. ../dists/ #  取出编译完毕的内容
 sudo rsync -av --delete ../dists/. /var/www/html #  博客内容暂时先当首页
 ls /var/www/html
+sudo nginx -s reload
 #sudo systemctl restart apache2
 sudo rm -rf ../dists/
